04123233fdae083b17f960550dbdd871029e297b
pchitwood2 <142836574+pchitwood2@users.noreply.github.com>
2023-08-31 12:19:44 -0400

    Add files via upload

---
+++ MO2LAB_CaseStudyAPP.py
@@ -0,0 +1,27 @@
+#Peyton Chitwood
+
+#MO2LAB_CaseStudyAPP.py
+
+#This application checks students. Users enter a last name and first name
+#for the student. If last name is ZZZ, the program quits. Users will also enter
+#GPA of students, and the GPAs are compared to see if the student makes the 
+#Dean's List or Honor Roll.
+
+#Variable Descriptions:
+#   lastName - String Value, stores last name of student via user input
+#   firstName - String Value, stores first name of student via user input
+#   GPA - Float Value, stores the student's GPA via user input
+
+while True:
+    lastName = input("Enter student's LAST name:")
+    if lastName == 'ZZZ':
+        print("PROCESS STOPPED")
+        break
+    firstName = input("Enter student's FIRST name:")
+    GPA = float(input("Enter GPA:"))
+    if GPA > 3.5:
+        print(firstName + " " + lastName + " has made the Dean's List")
+    elif GPA > 3.25 and GPA < 3.5:
+        print(firstName + " " + lastName + " has made the Honor Roll")
+    else:
+        print(firstName + " " + lastName + " has a GPA of " + str(GPA))
